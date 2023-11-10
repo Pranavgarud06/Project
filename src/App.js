f33bb99535f333c3953bb7054359e0f2b381c1fb
Pranavgarud06 <135837711+Pranavgarud06@users.noreply.github.com>
2023-11-10 08:06:51 -0800

    Add files via upload
    React js Project 2

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
+import Header from "./components/Header/Header";
+import Form from "./components/Form/UserForm";
+import Result from "./components/ResultTable/Result";
 import { useState } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpenses/NewExpenses";
-
-const Initial_items = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount:100,
-    date: new Date(2020, 9, 14),
-  },
-  { 
-    id: 'e2',
-   title: 'New TV',
-    amount: 50000, 
-    date: new Date(2022, 1, 12) 
-  },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 2000,
-    date: new Date(2019, 4, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2022, 6, 12),
-  },
-];
-
 function App() {
-  const[expenses, setExpenses] = useState(Initial_items);
+  const [userInput, setUserInput] = useState(null);
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
 
-  function addExpenseHandler (expense) {
-    setExpenses (prevExpenses => {
-      return [expense, ...prevExpenses];
-    });
   };
 
-  return <div>
-   <NewExpense  onAddExpense={addExpenseHandler}/>
-  <Expenses item={expenses} />
-  
-  </div>
+  const yearlyData = [];
+  if (userInput) {
+    let currentSavings = +userInput['current-savings'];
+    const yearlyContribution = +userInput['yearly-contribution'];
+    const expectedReturn = +userInput['expected-return'] / 100;
+    const duration = +userInput['duration'];
 
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <Form onCalculate={calculateHandler} />
+      {!userInput && <p style={{textAlign: "center"}}>No investment calculated yet.</p>}
+      {userInput &&  <Result info ={yearlyData} initialInvestment = {userInput['current-savings']}/>}
+    </div>
+  );
 }
+
 export default App;
